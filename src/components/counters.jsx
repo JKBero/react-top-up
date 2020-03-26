d3f58dd99d3c0b3f1ad073a2a689aae42c025d6b
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-03-26 20:47:13 +0000

    feat: convert Counters component to a stateless functional component

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Counter from './counter';
 
-class Counters extends Component {
-  render() {
-    return (
-      <div>
-        <button
-          onClick={this.props.onReset}
-          className="btn btn-primary btn-sm m-2"
-        >
-          Reset
-        </button>
-        {this.props.counters.map(counter =>
-          <Counter
-            key={counter.id}
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
-            counter={counter}
-          />
-        )}
-      </div>
-    );
-  }
+const Counters = props => {
+  return (
+    <div>
+      <button
+        onClick={props.onReset}
+        className="btn btn-primary btn-sm m-2"
+      >
+        Reset
+      </button>
+      {props.counters.map(counter =>
+        <Counter
+          key={counter.id}
+          onDelete={props.onDelete}
+          onIncrement={props.onIncrement}
+          counter={counter}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Counters;
